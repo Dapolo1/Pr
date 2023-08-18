90240353f24490e72ca51eb3c345fc61962d7c41
Daniel Polo <danielandrespolooviedo08@gmail.com>
2023-08-17 21:30:24 -0500

    first commit

---
+++ tara.py
@@ -0,0 +1,9 @@
+# Suma de numeros N
+
+print("Digite un numero:")
+a = int(input())
+b = 0
+for i in range(1, a + 1):
+    print(i)
+    b = b + 1
+    print("la suma de los numeros es:", b)
---
+++ tarea.py
@@ -0,0 +1,16 @@
+# Variable int
+num_int = 15
+print(num_int)
+
+# Variablre float
+num_float = 8.5
+print(num_float)
+
+# Variable Boleanos
+es_bajo = ("true")
+print(es_bajo)
+
+# Variable string
+num1 = 20
+num2 = 30
+print(f"La suma de los numeros {num1} y {num2} es {num1+num2}")
